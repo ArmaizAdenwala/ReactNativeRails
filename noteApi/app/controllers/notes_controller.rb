23f27c5449d96cfad55502c8700bfb4f02db3aca
Armaiz Adenwala <armaiz.adenwala@gmail.com>
2019-09-23 09:57:03 -0700

    setup endpoints

--- noteApi/app/controllers/notes_controller.rb
+++ noteApi/app/controllers/notes_controller.rb
@@ -1,2 +1,17 @@
 class NotesController < ApplicationController
+  def index
+    notes = Note.all
+    render json: notes, status: :ok
+  end
+
+  def create
+    note = Note.create!(note_params)
+    render json: note, status: :ok
+  end
+
+  def note_params
+    params.require(:note).permit(
+      :text,
+    )
+  end
 end
